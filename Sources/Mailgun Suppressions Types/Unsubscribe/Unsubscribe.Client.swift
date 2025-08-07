1da62b7397bbf7da98b258b3d77e6508fa1c164b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-08-07 07:14:03 +0200

    refactor: Update Suppressions Unsubscribe API to v3 specification with namespace pattern

--- Sources/Mailgun Suppressions Types/Unsubscribe/Unsubscribe.Client.swift
+++ Sources/Mailgun Suppressions Types/Unsubscribe/Unsubscribe.Client.swift
@@ -12,21 +12,21 @@ extension Mailgun.Suppressions.Unsubscribe {
     @DependencyClient
     public struct Client: Sendable {
         @DependencyEndpoint
-        public var get: @Sendable (_ address: EmailAddress) async throws -> Mailgun.Suppressions.Unsubscribe.Record
+        public var importList: @Sendable (_ request: Foundation.Data) async throws -> Mailgun.Suppressions.Unsubscribe.Import.Response
+        
+        @DependencyEndpoint
+        public var get: @Sendable (_ address: EmailAddress) async throws -> Mailgun.Suppressions.Unsubscribe.Get.Response
 
         @DependencyEndpoint
         public var delete: @Sendable (_ address: EmailAddress) async throws -> Mailgun.Suppressions.Unsubscribe.Delete.Response
 
         @DependencyEndpoint
-        public var list: @Sendable (_ request: Mailgun.Suppressions.Unsubscribe.List.Request) async throws -> Mailgun.Suppressions.Unsubscribe.List.Response
+        public var list: @Sendable (_ request: Mailgun.Suppressions.Unsubscribe.List.Request?) async throws -> Mailgun.Suppressions.Unsubscribe.List.Response
 
         @DependencyEndpoint
         public var create: @Sendable (_ request: Mailgun.Suppressions.Unsubscribe.Create.Request) async throws -> Mailgun.Suppressions.Unsubscribe.Create.Response
 
         @DependencyEndpoint
-        public var deleteAll: @Sendable () async throws -> Mailgun.Suppressions.Unsubscribe.Delete.All.Response
-
-        @DependencyEndpoint
-        public var importList: @Sendable (_ request: Foundation.Data) async throws -> Mailgun.Suppressions.Unsubscribe.Import.Response
+        public var deleteAll: @Sendable () async throws -> Mailgun.Suppressions.Unsubscribe.DeleteAll.Response
     }
 }
